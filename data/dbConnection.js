989845002d80b68bf83516ffcffb55688cef85b4
Luiz Antonio Dos Santos <102430261+luiz01204@users.noreply.github.com>
2025-02-27 14:51:34 -0300

    Update dbConnection.js

--- data/dbConnection.js
+++ data/dbConnection.js
@@ -1,6 +1,7 @@
 const sequelize = require("sequelize");
+require('dotenv').config();
 
-const connection = new sequelize("api_games","root","029599012",{
+const connection = new sequelize("process.env.DB_NAME","process.env.DB_USER","process.env.DB_PASS",{
     host: "localhost",
     dialect: "mysql",
     timezonetimezone: "-03:00"
